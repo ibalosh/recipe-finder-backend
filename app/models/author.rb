478b90e18d83c89a69013c2fd515ce1be5e8778f
Igor Balos <ibalosh@gmail.com>
2025-06-01 09:18:13 +0200

    connect back the relations in model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
 class Author < ApplicationRecord
+  has_many :recipes
+
   validates :name, presence: true, length: { minimum: 1, maximum: 50 }
 end
--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,3 +1,5 @@
 class Cuisine < ApplicationRecord
+  has_many :recipes
+
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 end
